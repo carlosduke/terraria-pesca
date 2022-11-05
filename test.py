546987bc630ce899a2d34cf0e29a11df1b7da48e
Carlos Roberto <carlos.dukep@gmail.com>
2022-11-05 13:08:42 -0300

    Config de ambientes e limites de cor

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from tkinter import filedialog
 import screeninfo
 import cv2
 import numpy as np
+import json
 
 from PIL import Image
 from PIL import ImageTk
@@ -21,6 +22,7 @@ def monitor_loop(app):
     while app.running:
         pos = pyautogui.position()
         app.update(pos.x, pos.y)
+        app.process_image(app.image)
 
         time.sleep(1/app.frames)
     print('stop loop')
@@ -53,13 +55,17 @@ class Application:
 
         self.running = False
         self.recording = False
+        self.fishing = False
         self.frames = 30
         self.cap = None
 
+        self.env_name = tkinter.StringVar()
+        self.env_name.set('--select--')
+        self.envs = ['DEFAULT']
         self.load_config()
         self.mount_gui(master)
 
-        self.image_path = 'C:/Users/carlo/Documents/desenv/projs/terraria-pesca/20221103T213720.mp4'
+        #self.image_path = 'C:/Users/carlo/Documents/desenv/projs/terraria-pesca/20221103T213720.mp4'
         self.load_image()
 
     def mount_gui(self, master=None):
@@ -101,6 +107,29 @@ class Application:
         self.bstop_monitor['command'] = lambda: stop_monitor(self)
         self.bstop_monitor["state"] = 'enable' if self.running else 'disabled'
         self.bstop_monitor.pack(side='left')
+
+        #Btn Recorder stop
+        self.bfishing = tkinter.Button(self.fcontrol)
+        self.bfishing["text"] = 'Fishing *' if self.fishing else 'Fishing'
+        self.bfishing['command'] = lambda: self.handle_fishing_btn()
+        self.bfishing.pack(side='left')
+
+        #Configs
+        self.fconfigenv = tkinter.Frame(self.fconfig)
+        self.fconfigenv.pack()
+
+        self.cenv = tkinter.ttk.Combobox(self.fconfigenv)
+        self.cenv['values'] = self.envs
+        self.cenv['textvariable'] = self.env_name
+        self.cenv.bind('<<ComboboxSelected>>', lambda e: self.change_env(e))
+        self.cenv.pack(side = 'left')
+
+        self.baddenv = tkinter.Button(self.fconfigenv)
+        self.baddenv['text'] = 'Add'
+        self.baddenv['command'] = lambda: self.add_env(
+            tkinter.simpledialog.askstring(title="Environment", prompt="Env name:")
+        )
+        self.baddenv.pack(side = 'left')
 
         #Widget itens
         self.fcomponents = tkinter.Frame(self.fconfig)
@@ -224,24 +253,98 @@ class Application:
         self.bconfig['text'] = 'Configure'
         self.bconfig['command'] = lambda: self.configure()
         self.bconfig.pack()
+    
+    def add_env(self, env_name):
+        if env_name not in self.envs:
+            self.envs.append(env_name)
         
+        self.cenv['values'] = self.envs
+        #self.cenv.insert(tkinter.END, env_name)
+
     def mount_preview(self, master=None):
         self.fpreview = tkinter.Frame(master)
         self.fpreview.pack(side='left')
 
-        self.sframes = tkinter.Scale(self.fpreview, from_=0, to=0, orient=tkinter.VERTICAL)
+        self.fpreview_imgs = tkinter.Frame(self.fpreview)
+        self.fpreview_imgs.pack()
+
+        self.sframes = tkinter.Scale(self.fpreview_imgs, from_=0, to=0, orient=tkinter.VERTICAL)
         self.sframes['command'] = lambda e: self.handle_configure()
         self.sframes['tickinterval']=1
         self.sframes.pack(side = 'left')
 
-        self.limage_orig = tkinter.Label(self.fpreview)
+        self.limage_orig = tkinter.Label(self.fpreview_imgs)
         self.limage_orig.pack(side = 'left')
 
-        self.limage_gray = tkinter.Label(self.fpreview)
+        self.limage_gray = tkinter.Label(self.fpreview_imgs)
         self.limage_gray.pack(side = 'left')
 
-        self.limage_process = tkinter.Label(self.fpreview)
+        self.limage_process = tkinter.Label(self.fpreview_imgs)
         self.limage_process.pack(side = 'left')
+
+        #ranges
+        #Lower
+        self.frange_lower = tkinter.Frame(self.fpreview)
+        self.frange_lower.pack(side='left')
+
+        #Red
+        self.frange_lower_r = tkinter.Frame(self.frange_lower)
+        self.frange_lower_r.pack()
+
+        self.srange_lower_r = tkinter.Scale(self.frange_lower_r, from_=0, to=255, orient=tkinter.HORIZONTAL)
+        self.srange_lower_r['command'] = lambda e: self.handle_configure()
+        self.srange_lower_r.set(self.boundary_lower[0])
+        self.srange_lower_r.pack()
+
+        #Green
+        self.frange_lower_g = tkinter.Frame(self.frange_lower)
+        self.frange_lower_g.pack()
+
+        self.srange_lower_g = tkinter.Scale(self.frange_lower_g, from_=0, to=255, orient=tkinter.HORIZONTAL)
+        self.srange_lower_g['command'] = lambda e: self.handle_configure()
+        self.srange_lower_g.set(self.boundary_lower[1])
+        self.srange_lower_g.pack()
+
+        #Blue
+        self.frange_lower_b = tkinter.Frame(self.frange_lower)
+        self.frange_lower_b.pack()
+
+        self.srange_lower_b = tkinter.Scale(self.frange_lower_b, from_=0, to=255, orient=tkinter.HORIZONTAL)
+        self.srange_lower_b['command'] = lambda e: self.handle_configure()
+        self.srange_lower_b.set(self.boundary_lower[2])
+        self.srange_lower_b.pack()
+
+
+        #Upper
+        self.frange_upper = tkinter.Frame(self.fpreview)
+        self.frange_upper.pack(side='left')
+
+        #Red
+        self.frange_upper_r = tkinter.Frame(self.frange_upper)
+        self.frange_upper_r.pack()
+
+        self.srange_upper_r = tkinter.Scale(self.frange_upper_r, from_=0, to=255, orient=tkinter.HORIZONTAL)
+        self.srange_upper_r['command'] = lambda e: self.handle_configure()
+        self.srange_upper_r.set(self.boundary_upper[0])
+        self.srange_upper_r.pack()
+
+        #Green
+        self.frange_upper_g = tkinter.Frame(self.frange_upper)
+        self.frange_upper_g.pack()
+
+        self.srange_upper_g = tkinter.Scale(self.frange_upper_g, from_=0, to=255, orient=tkinter.HORIZONTAL)
+        self.srange_upper_g['command'] = lambda e: self.handle_configure()
+        self.srange_upper_g.set(self.boundary_upper[1])
+        self.srange_upper_g.pack()
+
+        #Blue
+        self.frange_upper_b = tkinter.Frame(self.frange_upper)
+        self.frange_upper_b.pack()
+
+        self.srange_upper_b = tkinter.Scale(self.frange_upper_b, from_=0, to=255, orient=tkinter.HORIZONTAL)
+        self.srange_upper_b['command'] = lambda e: self.handle_configure()
+        self.srange_upper_b.set(self.boundary_upper[2])
+        self.srange_upper_b.pack()
 
         self.load_image()
         self.process_image(self.image)
@@ -280,13 +383,14 @@ class Application:
             img = cv2.imread(self.image_path)
             img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         elif self.image_path.lower().endswith('.mp4'):
+            start = 3980
             if not self.cap:
                 self.cap = cv2.VideoCapture(self.image_path)
 
                 #print((self.cap.get(cv2.CAP_PROP_FRAME_COUNT)))
-                self.sframes['to'] = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT)) -1
+                self.sframes['to'] = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT)) -1 - start
 
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.sframes.get())
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, start + self.sframes.get())
             ok, img = self.cap.read()
             img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         
@@ -331,82 +435,75 @@ class Application:
 
     def process_image(self, image):
         g1,g2 = self.gaussianblur
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        try:
-            gray = cv2.GaussianBlur(gray, (g1,g2), 0)
-        except Exception as e:
-            print('Error to GaussianBlur', e)
+        lower = np.array(self.boundary_lower, dtype='uint8')
+        upper = np.array(self.boundary_upper, dtype='uint8')
 
-        # # loop over the boundaries
-        # for (lower, upper) in boundaries:
-        #     # create NumPy arrays from the boundaries
-        #     lower = np.array(lower, dtype = "uint8")
-        #     upper = np.array(upper, dtype = "uint8")
-        #     # find the colors within the specified boundaries and apply
-        #     # the mask
-        #     mask = cv2.inRange(image, lower, upper)
-        #     output = cv2.bitwise_and(image, image, mask = mask)
-        #     # show the images
-        #     cv2.imshow("images", np.hstack([image, output]))
-        #     cv2.waitKey(0)
-        boundaries = [
-            ([17, 15, 100], [50, 56, 200]),
-            ([86, 31, 4], [220, 88, 50]),
-            ([25, 146, 190], [62, 174, 250]),
-            ([103, 86, 65], [145, 133, 128])
-        ]
-        l,u = boundaries[0]
-        lower = np.array(l, dtype='uint8')
-        upper = np.array(u, dtype='uint8')
         #proc = cv2.GaussianBlur(image, (g1,g2), 0)
         proc = cv2.inRange(image, lower, upper)
+        proc = cv2.bitwise_and(image, image, mask= proc)
+
+        image_proc = proc
+        img_proc = Image.fromarray(image_proc)
+        img_proc = ImageTk.PhotoImage(img_proc)
+        self.limage_gray.configure(image=img_proc)
+        self.limage_gray.image = img_proc
+
+        proc = cv2.cvtColor(proc, cv2.COLOR_BGR2GRAY)
+        th = self.threshold
+        th, proc = cv2.threshold(proc, th, 255, cv2.THRESH_BINARY)
+
+        #image_proc = np.hstack((image_proc, proc))
         #proc = image
         #cv2.circle(proc, (125,50), 30, (191,24,3), -1)
         #cv2.circle(proc, (125,150), 30, (228,29,3), -1)
         # th = self.threshold
         # th, proc = cv2.threshold(gray, th, 255, cv2.THRESH_BINARY)
         
-        gray = Image.fromarray(gray)
-        gray = ImageTk.PhotoImage(gray)
-        self.limage_gray.configure(image=gray)
-        self.limage_gray.image = gray
+        # gray = Image.fromarray(gray)
+        # gray = ImageTk.PhotoImage(gray)
+        # self.limage_gray.configure(image=gray)
+        # self.limage_gray.image = gray
 
         p = Image.fromarray(proc)
         p = ImageTk.PhotoImage(p)
         self.limage_process.configure(image=p)
         self.limage_process.image = p
 
-        # if len(self.buffer) == 0:
-        #     for i in range(0, 3):
-        #         self.buffer.append(proc)
+        if len(self.buffer) == 0:
+            for i in range(0, 3):
+                self.buffer.append(proc)
         
-        # self.buffer_idx = (self.buffer_idx + 1)%len(self.buffer)
-        # self.buffer[self.buffer_idx] = proc
-        # t0, t1, t2 = self.buffer
-        # d1 = cv2.absdiff(t0, t1)
-        # d2 = cv2.absdiff(t1,t2)
-        # res = cv2.bitwise_or(d1,d2)
-        # count = cv2.countNonZero(res)
-        # if count == 0: count = 1
+        self.buffer_idx = (self.buffer_idx + 1)%len(self.buffer)
+        self.buffer[self.buffer_idx] = proc
+        t0, t1, t2 = self.buffer
+        d1 = cv2.absdiff(t0, t1)
+        d2 = cv2.absdiff(t1,t2)
+        res = cv2.bitwise_or(d1,d2)
+        #print(res.shape)
+        try:
+            count = cv2.countNonZero(res)
+        except:
+            count = 1
+        if count == 0: count = 1
         
-        # if self.count == -1:
-        #     self.count = count
+        if self.count == -1:
+            self.count = count
         
-        # if not self.running:
-        #     return
-        # diff = abs(self.count - count)
-        # self.lstatus['text'] = f'{self.sense} - {diff}, {(time.time() - self.last_sent)}'
+        if not self.running or not self.fishing:
+            return
+        diff = abs(self.count - count)
+        self.lstatus['text'] = f'{self.sense} - {diff}, {(time.time() - self.last_sent)}'
 
-        # if diff > self.sense and (time.time() - self.last_sent) > 3:
-        #     self.last_sent = time.time()
-        #     pyautogui.mouseDown()
-        #     time.sleep(0.02)
-        #     pyautogui.mouseUp()
+        if diff > self.sense and (time.time() - self.last_sent) > 3:
+            self.last_sent = time.time()
+            pyautogui.mouseDown()
+            time.sleep(0.02)
+            pyautogui.mouseUp()
 
-        #     time.sleep(0.5)
-        #     pyautogui.mouseDown()
-        #     time.sleep(0.02)
-        #     pyautogui.mouseUp()
+            time.sleep(0.5)
+            pyautogui.mouseDown()
+            time.sleep(0.02)
+            pyautogui.mouseUp()
 
         # res = Image.fromarray(res)
         # res = ImageTk.PhotoImage(res)
@@ -422,55 +519,87 @@ class Application:
         self.sposx['to'] = wscreen
         self.fposy['to'] = hscreen
 
+    def change_env(self, e):
+        print(self.env_name.get(), self.envs)
+
+        self.load_config()
+
+        self.srange_lower_r.set(self.boundary_lower[0])
+        self.srange_lower_g.set(self.boundary_lower[1])
+        self.srange_lower_b.set(self.boundary_lower[2])
+
+        self.srange_upper_r.set(self.boundary_upper[0])
+        self.srange_upper_g.set(self.boundary_upper[1])
+        self.srange_upper_b.set(self.boundary_upper[2])
+
+        self.configure()
+
     def load_config(self):
         self.config = configparser.ConfigParser()
         self.config.read('./config.ini')
-        config_name = 'DEFAULT'
+
+        self.envs = self.config.sections()
+        #TODO: Verificar
+        if 'DEFAULT' not in self.envs:
+            self.envs.append('DEFAULT')
+
+        cur_env = self.env_name.get()
+        if cur_env not in self.envs:
+            cur_env = 'DEFAULT'
 
         self.size = (
-            int(self.config[config_name].get('size_with', 350)),
-            int(self.config[config_name].get('size_height', 350)),
+            int(self.config[cur_env].get('size_with', 350)),
+            int(self.config[cur_env].get('size_height', 350)),
         )
 
-        self.threshold = int(self.config[config_name].get('threshold', 350))
+        self.threshold = int(self.config[cur_env].get('threshold', 350))
         self.gaussianblur = (
-            int(self.config[config_name].get('gaussianblur_v1', 350)),
-            int(self.config[config_name].get('gaussianblur_v2', 350))
+            int(self.config[cur_env].get('gaussianblur_v1', 350)),
+            int(self.config[cur_env].get('gaussianblur_v2', 350))
         )
         self.pos = (
-            int(self.config[config_name].get('screen_x', 0)),
-            int(self.config[config_name].get('screen_y', 0))
+            int(self.config[cur_env].get('screen_x', 0)),
+            int(self.config[cur_env].get('screen_y', 0))
         )
         self.image_path = None
-        self.view_size = int(self.config[config_name].get('view_size', 50))
+        self.view_size = int(self.config[cur_env].get('view_size', 50))
 
         self.screens = screeninfo.get_monitors()
-        self.screen_idx = int(self.config[config_name].get('screen_idx', 0))
+        self.screen_idx = int(self.config[cur_env].get('screen_idx', 0))
 
-        self.sense = int(self.config[config_name].get('sense', 0))
+        self.sense = int(self.config[cur_env].get('sense', 0))
+        self.boundary_lower = json.loads(self.config[cur_env].get('boundary_lower', '[0,0,0]'))
+        self.boundary_upper = json.loads(self.config[cur_env].get('boundary_upper', '[255,255,255]'))
 
-        #print(self.pos)
+        #print(cur_env, self.boundary_lower, self.boundary_upper)
 
     def save_config(self):
-        config_name = 'DEFAULT'
-
+        cur_env = self.env_name.get()
+        #print('Start save', cur_env, self.envs)
+        if cur_env == '--select--': return
+        if cur_env not in self.envs:
+            self.config.add_section(cur_env)
+        
         w,h = self.size
-        self.config[config_name]['size_with'] = str(w)
-        self.config[config_name]['size_height'] = str(h)
+        self.config[cur_env]['size_with'] = str(w)
+        self.config[cur_env]['size_height'] = str(h)
 
-        self.config[config_name]['threshold'] = str(self.threshold)
+        self.config[cur_env]['threshold'] = str(self.threshold)
 
         g1,g2 = self.gaussianblur
-        self.config[config_name]['gaussianblur_v1'] = str(g1)
-        self.config[config_name]['gaussianblur_v2'] = str(g2)
+        self.config[cur_env]['gaussianblur_v1'] = str(g1)
+        self.config[cur_env]['gaussianblur_v2'] = str(g2)
 
         x,y = self.pos
-        self.config[config_name]['screen_x'] = str(x)
-        self.config[config_name]['screen_y'] = str(y)
-        self.config[config_name]['view_size'] = str(self.view_size)
+        self.config[cur_env]['screen_x'] = str(x)
+        self.config[cur_env]['screen_y'] = str(y)
+        self.config[cur_env]['view_size'] = str(self.view_size)
 
-        self.config[config_name]['screen_idx'] = str(self.screen_idx)
-        self.config[config_name]['sense'] = str(self.sense)
+        self.config[cur_env]['screen_idx'] = str(self.screen_idx)
+        self.config[cur_env]['sense'] = str(self.sense)
+
+        self.config[cur_env]['boundary_lower'] = str(self.boundary_lower)
+        self.config[cur_env]['boundary_upper'] = str(self.boundary_upper)
 
         with open('./config.ini', 'w') as f:
             self.config.write(f)
@@ -503,7 +632,18 @@ class Application:
         self.sense = int(self.esense.get())
 
         #print(self.size, self.threshold, self.gaussianblur, self.pos)
+        self.boundary_lower = [
+            self.srange_lower_r.get(),
+            self.srange_lower_g.get(),
+            self.srange_lower_b.get()
+        ]
 
+        self.boundary_upper = [
+            self.srange_upper_r.get(),
+            self.srange_upper_g.get(),
+            self.srange_upper_b.get()
+        ]
+        
     def start_record(self):
         self.video_writer = cv2.VideoWriter(
             './' + datetime.now().strftime('%Y%m%dT%H%M%S') + '.mp4',
@@ -523,7 +663,6 @@ class Application:
         x,y = self.pos
         self.update(x,y)
 
-
     def update(self, x = 0,y = 0):
         self.lstatus['text'] = f'X: {x}, Y: {y}'
 
@@ -536,7 +675,10 @@ class Application:
             self.video_writer.write(cv2.cvtColor(self.image, cv2.COLOR_RGB2BGR))
 
         #self.process_image(self.image)
-        
+
+    def handle_fishing_btn(self):
+        self.fishing = not self.fishing
+        self.bfishing["text"] = 'Fishing *' if self.fishing else 'Fishing'
 
 
 def verify_image(img):
